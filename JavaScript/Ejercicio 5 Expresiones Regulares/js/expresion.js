8b35b031081e6f99480fd459305907f0fb9b9e6a
unknown <FPE@965PDS3-100.aeg8.edu>
2015-03-06 13:02:16 +0100

    js

--- JavaScript/Ejercicio 5 Expresiones Regulares/js/expresion.js
+++ JavaScript/Ejercicio 5 Expresiones Regulares/js/expresion.js
@@ -1,5 +1,31 @@
+/* Encontrar la fecha dentro de la frase */
+
 var text = 'Nací el 05/04/1982 en Donostia.';
 
-var tags = /^([0][1-9]|[12][0-9]|3[01])(\/|-)([0][1-9]|[1][0-2])\2(\d{4})$/;
+var expresion = /\d{2}\/\d{2}\/\d{4}/;  
+
+/* 
+
+^ el simbolo indica que debe comenzar por.
+$ que debe acabar en.
+
+*/
  
-var respuestabool = tags.test(text);
+/* Validar el texto introducido con la expresion */
+var respuestabool = expresion.test(text);
+
+console.log(respuestabool);
+
+
+
+//
+
+var email= "anderyuste@gmail.com";
+
+var expresionmail = /\w+@[a-zA-Z0-9-]+?\.[a-zA-Z0-9-]{2,10};
+
+var respuestamail = expresionmail.test(email);
+
+console.log(expresionmail);
+                                                                       
+                                                                       
