93111ca7456a18e647420e7fb3af91d4c56c002a
pjrn <anderyuste@gmail.com>
2015-03-09 00:27:49 +0100

    change

--- Proyecto DAM/js/funciones.js
+++ Proyecto DAM/js/funciones.js
@@ -6,3 +6,24 @@ function mostrar() {
         elemento.className = "hidden";
     }
 }
+
+function addElement(productotext) {
+
+    var borrar = document.createElement('input');
+    borrar.setAttribute('type', 'button');
+    borrar.setAttribute('value', 'X');
+    borrar.setAttribute('class', 'borrar');
+    /* borrar.setAttribute('onclick', 'removeChild(node);'); */
+
+
+    var productonuevo = document.getElementById("listadelacompra");
+    var insertar = document.createElement("p");
+    insertar.innerHTML = productotext;
+    productonuevo.appendChild(insertar);
+    productonuevo.appendChild(borrar);
+
+    document.getElementById("productotext").value = "";
+    document.getElementById("productotext").focus();
+
+
+}
