3f6d9ecd6192110499364771e0dd9f94009420a8
pjrn <anderyuste@gmail.com>
2015-03-08 02:03:04 +0100

    primeros pasos

--- Proyecto DAM/js/funciones.js
+++ Proyecto DAM/js/funciones.js
@@ -1,8 +1,8 @@
 function mostrar() {
     var elemento = document.getElementById("oculto");
     if (elemento.className == "hidden") {
-      elemento.className = "nohidden";
-    }else {
-      elemento.className = "hidden";
+        elemento.className = "nohidden";
+    } else {
+        elemento.className = "hidden";
     }
 }
