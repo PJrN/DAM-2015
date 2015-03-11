fa5aad065e58dfd1d4c31acf675be525c6662859
pjrn <anderyuste@gmail.com>
2015-03-11 21:59:37 +0100

    change

--- Ajax/Ejercicio7combosJSON/js/code.js
+++ Ajax/Ejercicio7combosJSON/js/code.js
@@ -1,98 +1,112 @@
-window.onload = function () {
-    "use strict";
+window.onload = function(){
 
-    /* al pulsar el enlace  "Comprobar", se debe comprobar el valor del input "login" con la respuesta del servidor "$numeroAleatorio" */
-    //Evento.
+    /* 1- Al cargar el documento rellenar las provincias: */
+    cargarProvincias();
+    /* 2- En el evento onchange Provincias rellenar municipios: */
+    document.getElementById("provincia").onchange = cargarMunicipios;
+};
 
-    var READY_STATE_UNINITIALIZED = 0;
-    var READY_STATE_LOADING = 1;
-    var READY_STATE_LOADED = 2;
-    var READY_STATE_INTERACTIVE = 3;
-    var READY_STATE_COMPLETE = 4;
+function cargarProvincias(){
 
-    var peticion_http;
-    var http_request;
-
-
-    /*
-XMLHttpRequest:
-La interfaz se implementa como una clase de la que una aplicación cliente puede generar tantas instancias como necesite para manejar el        diálogo con el servidor.
-
-El uso más popular, si bien no el único, de esta interfaz es proporcionar contenido dinámico y actualizaciones asíncronas en páginas WEB mediante tecnologías construidas sobre ella como por ejemplo AJAX.
-*/
-
-
-    function inicializa_xhr() {
-        if (window.XMLHttpRequest) {
-            return new XMLHttpRequest();
-        } else if (window.ActiveXObject) {
-            return new ActiveXObject("Microsoft.XMLHTTP");
-        }
+    if(window.XMLHttpRequest) {
+        var peticion_http = new XMLHttpRequest();
     }
-
-
-    function rellenar() {
-        peticion_http = inicializa_xhr();
-        if (peticion_http) {
-            peticion_http.onreadystatechange = respuesta;
-            peticion_http.open("POST", "http://localhost/Ejercicios/Ajax/Ejercicio7combosJSON/servidor/cargaProvinciasJSON.php", true);
-
-            peticion_http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            var query_string = crea_query_string();
-            peticion_http.send(query_string);
-        }
+    else if(window.ActiveXObject) {
+        var peticion_http = new ActiveXObject("Microsoft.XMLHTTP");
     }
+    peticion_http.onreadystatechange = provincias;
 
-    function rellenar2() {
-        peticion_http = inicializa_xhr();
-        if (peticion_http) {
-            peticion_http.onreadystatechange = respuesta;
-            peticion_http.open("POST", "http://localhost/Ejercicios/Ajax/Ejercicio7combosJSON/servidor/cargaMunicipiosJSON.php", true);
+    peticion_http.open('GET',"http://localhost/Ajax/Ejercicio7combosJSON/servidor/cargaProvinciasJSON.php" , true);
+    peticion_http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    peticion_http.send(null);
 
-            peticion_http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            var query_string = crea_query_string2();
-            peticion_http.send(query_string);
-        }
-    }
+    function provincias(){
 
-    function respuesta() {
-        if (peticion_http.readyState == READY_STATE_COMPLETE) {
-            if (peticion_http.status == 200) {
-                var respuesta_json = peticion_http.responseText;
+        if(peticion_http.readyState == 4) {
+            if(peticion_http.status == 200) {
 
+                parseaProvinciasJSON(peticion_http);
 
-                /* se debe transformar esa cadena de texto en un objeto JSON */
-                var objeto_json = JSON.parse(respuesta_json);
+            }else{
 
-                // Con JSON
-                // var pronvicias = objeto_json.codigo;
-
-                /* RECORRER EL P*** OBJETO DE JSON */
-
-                for (var i = 0; i < objeto_json.length; i++) {
-                    //console.log(objeto_json[i].nombre);
-                }
             }
         }
     }
 
-    /*
-    La función crea_query_string obtiene el valor de  los campos y los concatena junto con el nombre de cada parámetro para formar la cadena de       texto que se envía al servidor.
-    El uso de la función encodeURIComponent() es imprescindible para evitar problemas con algunos caracteres especiales.
-        */
 
-    function crea_query_string() {
+}
 
-        //buscas la variable dentro del php
-        return "codigo" + "&nocache=" + Math.random();
+function cargarMunicipios(){
+
+
+    if(window.XMLHttpRequest) {
+        var peticion_http = new XMLHttpRequest();
+    }
+    else if(window.ActiveXObject) {
+        var peticion_http = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    peticion_http.onreadystatechange = municipios;
+
+    peticion_http.open('POST',"http://localhost/Ajax/Ejercicio7combosJSON//servidor/cargaMunicipiosJSON.php" , true);
+    peticion_http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    var query_string = crea_query_string();
+    peticion_http.send(query_string);
+
+     function municipios(){
+
+        if(peticion_http.readyState == 4) {
+            if(peticion_http.status == 200) {
+
+                parseaMunicipiosJSON(peticion_http);
+
+            }else{
+
+            }
+        }
     }
 
-    function crea_query_string2() {
 
-        //buscas la variable dentro del php
-        return "nombre" + "&nocache=" + Math.random();
+}
+
+function parseaMunicipiosJSON(peticion_http){
+    var listaP = document.getElementById("municipio");
+    var respuesta_json = peticion_http.responseText;
+    var objeto_json = JSON.parse(respuesta_json);
+
+    var i =1;
+    for (var objeto in objeto_json){
+
+        listaP.options[i] = new Option(objeto_json[objeto],objeto);
+        i++;
+
     }
 
-    rellenar();
-    // rellenar2();
+}
+
+
+
+function parseaProvinciasJSON(peticion_http){
+    var listaP = document.getElementById("provincia");
+    var respuesta_json = peticion_http.responseText;
+    var objeto_json = JSON.parse(respuesta_json);
+    var i =1;
+    var code ="";
+    for (var objeto in objeto_json){
+
+        if(i<10){
+            code = "0"+i;
+        }else{
+            code = ""+i;
+        }
+        listaP.options[i] = new Option(objeto_json[code],code);
+        i++;
+
+    }
+
+}
+
+function crea_query_string() {
+
+        var provincia = document.getElementById("provincia").value;
+        return "provincia=" + encodeURIComponent(provincia) + "&nocache=" + Math.random();
 }
