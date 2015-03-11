aa7d6589b330f34ebe9eaf115a0d03dee85806c6
pjrn <anderyuste@gmail.com>
2015-03-11 22:24:36 +0100

    change

--- Proyecto DAM/js/funciones.js
+++ Proyecto DAM/js/funciones.js
@@ -7,23 +7,33 @@ function mostrar() {
     }
 }
 
+var contador = 1; //select id_producto from lista-compra +1 where fecha ;
+
 function addElement(productotext) {
 
+    
+    var fecha = new Date();
+    
     var borrar = document.createElement('input');
     borrar.setAttribute('type', 'button');
-    borrar.setAttribute('value', 'X');
+    borrar.setAttribute('name', contador + "-" + + fecha.getDate() + "-" + (fecha.getMonth() +1) + "-" + fecha.getFullYear());
     borrar.setAttribute('class', 'borrar');
     /* borrar.setAttribute('onclick', 'removeChild(node);'); */
 
 
     var productonuevo = document.getElementById("listadelacompra");
     var insertar = document.createElement("p");
+    insertar.setAttribute ('name', contador + "-" + fecha.getDate() + "-" + (fecha.getMonth() +1) + "-" + fecha.getFullYear());
     insertar.innerHTML = productotext;
     productonuevo.appendChild(insertar);
     productonuevo.appendChild(borrar);
 
     document.getElementById("productotext").value = "";
     document.getElementById("productotext").focus();
+    
+    contador++;
+    
+    
 
 
 }
