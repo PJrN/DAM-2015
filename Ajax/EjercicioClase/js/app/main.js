ac60fb737eca9c2e02a995ba3aaa32aeb44142c1
unknown <FPE@965PDS3-100.aeg8.edu>
2015-03-13 09:19:58 +0100

    new

---
+++ Ajax/EjercicioClase/js/app/main.js
@@ -0,0 +1,28 @@
+//obeto global.
+
+//Usar "use strict".
+
+//esperar a que el DOM este cargado.
+
+
+// Iniciaremos las variables privadas que sean necesarias.
+
+// Necesitaremos una funcion que cree objetos DOM de clase img y todos los atributos necesarios.
+// Necesitaremos una funcion que agrege el array de img al DOM (no se realizaran cambios de DOM dentro de ningun bucle).
+
+// Necesitaremos una o varias funcion(es) que controle(n) el paso del tiempo.
+
+// Necesitaremos añadir los eventos necesarios en el momento adecuado.
+
+// Necesitaremos las funciones de callback para los eventos.
+
+// Necesitaremos una funcion encargada de llamar al modulo que se define en el fichero net.js con los parametros adecuados para realizar la llamada y capturar la respuesta AJAX.
+
+// Necesitaremos una funcion que controle el final del juego.
+
+// Necesitaremos una funcion que controle la lista de eliminados.
+
+// Cualquier otra funcion que sea necesaria.
+
+
+
---
+++ Ajax/EjercicioClase/js/app/net.js
@@ -0,0 +1,11 @@
+//obeto global
+
+//Usar "use strict"
+
+//esperar a que el DOM este cargado
+
+//Funcion anonima autoejecutable para realizar las peticiones AJAX, que añadiremos en el objeto global
+//y expondra un metodo para poder realizar las llamadas al servidor.
+
+//El servidor espera un parametro 'numero' con el número de elementos que tendra el JSON de respuesta.
+
