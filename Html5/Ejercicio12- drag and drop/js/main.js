984dad6f2118d7d8d0a0d7b1e4565f1ed29dc259
unknown <FPE@965PDS3-100.aeg8.edu>
2015-03-18 12:04:18 +0100

    change

---
+++ Html5/Ejercicio12- drag and drop/js/main.js
@@ -0,0 +1,55 @@
+function handleDragStart(e) {
+    this.style.opacity = '0.4';
+}
+ 
+function handleDragOver(e) {
+    if (e.preventDefault) {
+        e.preventDefault(); // Necessary. Allows us to drop.
+    }
+    e.dataTransfer.dropEffect = 'move';
+    return false;
+}
+ 
+function handleDragEnter(e) {
+    // this / e.target is the current hover target.
+    this.classList.add('over');
+}
+ 
+function handleDragLeave(e) {
+    this.classList.remove('over');  // this / e.target is previous target element.
+}
+ 
+var cols = document.querySelectorAll('.product');
+[].forEach.call(cols, function(col) {
+    col.addEventListener('dragstart', handleDragStart, false);
+    col.addEventListener('dragenter', handleDragEnter, false);
+    col.addEventListener('dragover', handleDragOver, false);
+    col.addEventListener('dragleave', handleDragLeave, false);
+});
+
+
+function handleDrop(e) {
+    // this / e.target is current target element.
+    if (e.stopPropagation) {
+        e.stopPropagation(); // stops the browser from redirecting.
+    }
+    // See the section on the DataTransfer object.
+    return false;
+}
+ 
+function handleDragEnd(e) {
+    // this/e.target is the source node.
+    [].forEach.call(cols, function (col) {
+        col.classList.remove('over');
+    });
+}
+ 
+var cols = document.querySelectorAll('#drop');
+[].forEach.call(cols, function(col) {
+    col.addEventListener('dragstart', handleDragStart, false);
+    col.addEventListener('dragenter', handleDragEnter, false);
+    col.addEventListener('dragover', handleDragOver, false);
+    col.addEventListener('dragleave', handleDragLeave, false);
+    col.addEventListener('drop', handleDrop, false);
+    col.addEventListener('dragend', handleDragEnd, false);
+});
