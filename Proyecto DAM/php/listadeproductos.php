08fd4b2842e516977566f1fb20bf00ca83424f6e
pjrn <anderyuste@gmail.com>
2015-03-20 17:22:44 +0100

    php insert

--- Proyecto DAM/php/listadeproductos.php
+++ Proyecto DAM/php/listadeproductos.php
@@ -1,24 +1,22 @@
 <?php
 
+//Include the connection:
 include("conexion.php");
 $link= Conectarse();
 
-$ultimoproducto= $_POST["ultiproducto"];
-echo($ultimoproducto);
-if (isset($_POST['ultimoproducto'])) $ultimoproducto= $_POST["ultimoproducto"];
-else{ $ultimoproducto= "0"; }
+//Return the last checked checkbox:
+$ultimoproducto= $_POST["ultimocheck"];
+//comprobar el valor:
+//echo($ultimoproducto);
 
-echo($ultimoproducto);
-
-
-
+//Estado always true:
 $estado = "1";
-$fecha = getdate();
-
-//Recibir por sesion.
+//Return date:
+$fecha = date('Y-m-d H:i:s');
+//Usuario pendiente de recibir por sesion:
 $id_usuario = "1";
 
-
+//Insert into Database:
 mysql_query("INSERT INTO producto(producto, fecha, estado, id_usuario)
 
 VALUES('$ultimoproducto', '$fecha', '$estado', '$id_usuario');",$link)or die("Error en la introducción de datos");
