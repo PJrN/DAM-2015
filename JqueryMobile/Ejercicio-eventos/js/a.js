65a1c4385c93aaab2242a1bd61486914ab27dc73
unknown <FPE@965PDS3-100.aeg8.edu>
2015-03-24 13:00:07 +0100

    jquery mobile change

--- JqueryMobile/Ejercicio-eventos/js/a.js
+++
@@ -1,9 +0,0 @@
-
-$(function(){
-  $( "div.noclick" ).bind( "tap", tapHandler );
-
-  function tapHandler( event ){
-    $( event.target ).toggleClass("noclick").addClass( "click" );
-      //toggleclass activa y desactiva
-  }
-});
