07486f6c167fed7dddad880c07f2d3bb53b5f2f9
PJrN <you@example.com>
2015-03-26 00:05:43 +0100

    change

--- JqueryMobile/Ejercicio-eventos/js/function.js
+++ JqueryMobile/Ejercicio-eventos/js/function.js
@@ -1,9 +1,68 @@
+$(function () {
+    // Bind the swipeHandler callback function to the swipe event on div.box
+    $("#pizzas").on("swiperight", desplazarse);
+    // Callback function references the event target and adds the 'swipe' class to it
+    function desplazarse(event) {
+        $.mobile.changePage("#pagina2", {
+            transition: "slide",
+            changeHash: false
+        });
+    }
+
+    $("#pizzas").on("swipeleft", desplazarsel);
+    // Callback function references the event target and adds the 'swipe' class to it
+    function desplazarsel(event) {
+        $.mobile.changePage("#pagina3", {
+            transition: "slide",
+            changeHash: false
+        });
+    }
+
+    $("#pagin2").on("swipeleft", desplazarsel);
+    // Callback function references the event target and adds the 'swipe' class to it
+    function desplazarsel(event) {
+        $.mobile.changePage("#pizzas", {
+            transition: "slide",
+            changeHash: false
+        });
+    }
+
+    $("#pagin2").on("swiperight", desplazarser);
+    // Callback function references the event target and adds the 'swipe' class to it
+    function desplazarser(event) {
+        $.mobile.changePage("#pagina3", {
+            transition: "slide",
+            changeHash: false
+        });
+    }
+
+    $("#pagin3").on("swipeleft", desplazarsel);
+    // Callback function references the event target and adds the 'swipe' class to it
+    function desplazarsel(event) {
+        $.mobile.changePage("#pagina2", {
+            transition: "slide",
+            changeHash: false
+        });
+    }
+
+    $("#pagin3").on("swiperight", desplazarser);
+    // Callback function references the event target and adds the 'swipe' class to it
+    function desplazarser(event) {
+        $.mobile.changePage("#pizzas", {
+            transition: "slide",
+            changeHash: false
+        });
+    }
+});
+
+
+
 $("[data-role='collapsible']").collapsible({
 
-    collapse: function( event, ui ) {
+    collapse: function (event, ui) {
         $(this).children().next().slideUp(350);
     },
-    expand: function( event, ui ) {
+    expand: function (event, ui) {
         $(this).children().next().hide();
         $(this).children().next().slideDown(350);
     }
