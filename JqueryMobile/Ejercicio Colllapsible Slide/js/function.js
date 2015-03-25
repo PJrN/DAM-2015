5d6f2f9036038d30bff9b6ca18e3600b2619e432
PJrN <you@example.com>
2015-03-25 21:16:02 +0100

    js

---
+++ JqueryMobile/Ejercicio Colllapsible Slide/js/function.js
@@ -0,0 +1,14 @@
+/*\
+Animate collapsible set;
+\*/
+$(document).one("pagebeforechange", function() {
+
+    // hide the other collapsibles first;
+          
+
+            // we do our own call to the original event;
+    
+     
+    });
+    
+    
