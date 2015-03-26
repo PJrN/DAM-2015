d8d332956ab856b58533bc3685b9c33f239923aa
unknown <FPE@965PDS3-100.aeg8.edu>
2015-03-26 14:00:40 +0100

    change

--- JqueryMobile/Ejercicio-eventos/js/function.js
+++ JqueryMobile/Ejercicio-eventos/js/function.js
@@ -1,3 +1,29 @@
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var arr = ["Margherita", "Napoli","Prosciutto", "Prosciutto + Funghi","Napoli","Prosciutto","Napoli","Prosciutto"];
+shuffle(arr);
+console.log(arr);
+console.log(arr[0]);
+$( "#decision" ).text(arr[0]);
+
+
 $(function () {
     // Bind the swipeHandler callback function to the swipe event on div.box
     $("#pizzas").on("swiperight", desplazarse);
