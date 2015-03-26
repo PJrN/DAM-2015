845013ef338958cce272d9c832119fabb3a6e439
PJrN <you@example.com>
2015-03-26 23:44:18 +0100

    js

--- JqueryMobile/Ejercicio-eventos/js/function.js
+++ JqueryMobile/Ejercicio-eventos/js/function.js
@@ -1,91 +1,94 @@
-$("#elegir").on( "click", function() {
+$("#elegir").on("click", function () {
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
+    function shuffle(array) {
+        var currentIndex = array.length,
+            temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+
+    var arr = ["Margherita", "Napoli", "Prosciutto", "Prosciutto + Funghi", "Tonno", "Frankfurt", "Pepperoni", "Funghi", "4 formaggi", "Bolognesa", "Vegetariana", "Parmigiana", "Barbakoa", "Brava", "4 stagioni", "Boscaiola", "diFreddy", "Per Tutti", "Potrue", "Difranky", "Mama Mia", "Berezia", "Carbonara", "Siciliana", "Tropikala", "Pesto"];
+    shuffle(arr);
+    console.log(arr);
+    console.log(arr[0]);
+    $("#decision").text(arr[0]);
+
+});
+
+$(function(){
+  // Bind the swipeHandler callback function to the swipe event on div.box
+  $( "#pizzas" ).on( "swipeleft", swiperleftindex );
+
+  // Callback function references the event target and adds the 'swipe' class to it
+  function swiperleftindex( event ){
+$.mobile.changePage( "#pagina3", { transition: "slide", changeHash: false });
   }
+});
 
-  return array;
-}
+$(function(){
+  // Bind the swipeHandler callback function to the swipe event on div.box
+  $( "#pizzas" ).on( "swiperight", swiperrightindex );
 
-var arr = ["Margherita", "Napoli","Prosciutto", "Prosciutto + Funghi","Tonno","Frankfurt","Pepperoni","Funghi","4 formaggi","Bolognesa","Vegetariana","Parmigiana","Barbakoa","Brava","4 stagioni","Boscaiola","diFreddy","Per Tutti","Potrue","Difranky","Mama Mia","Berezia","Carbonara","Siciliana","Tropikala","Pesto"];
-shuffle(arr);
-console.log(arr);
-console.log(arr[0]);
-$( "#decision" ).text(arr[0]);
-
-  });
-
-
-$(function () {
-    // Bind the swipeHandler callback function to the swipe event on div.box
-    $("#pizzas").on("swiperight", desplazarse);
-    // Callback function references the event target and adds the 'swipe' class to it
-    function desplazarse(event) {
-        $.mobile.changePage("#pagina2", {
-            transition: "slide",
-            changeHash: false
-        });
-    }
-
-    $("#pizzas").on("swipeleft", desplazarsel);
-    // Callback function references the event target and adds the 'swipe' class to it
-    function desplazarsel(event) {
-        $.mobile.changePage("#pagina3", {
-            transition: "slide",
-            changeHash: false
-        });
-    }
-
-    $("#pagin2").on("swipeleft", desplazarsel);
-    // Callback function references the event target and adds the 'swipe' class to it
-    function desplazarsel(event) {
-        $.mobile.changePage("#pizzas", {
-            transition: "slide",
-            changeHash: false
-        });
-    }
-
-    $("#pagin2").on("swiperight", desplazarser);
-    // Callback function references the event target and adds the 'swipe' class to it
-    function desplazarser(event) {
-        $.mobile.changePage("#pagina3", {
-            transition: "slide",
-            changeHash: false
-        });
-    }
-
-    $("#pagin3").on("swipeleft", desplazarsel);
-    // Callback function references the event target and adds the 'swipe' class to it
-    function desplazarsel(event) {
-        $.mobile.changePage("#pagina2", {
-            transition: "slide",
-            changeHash: false
-        });
-    }
-
-    $("#pagin3").on("swiperight", desplazarser);
-    // Callback function references the event target and adds the 'swipe' class to it
-    function desplazarser(event) {
-        $.mobile.changePage("#pizzas", {
-            transition: "slide",
-            changeHash: false
-        });
-    }
+  // Callback function references the event target and adds the 'swipe' class to it
+  function swiperrightindex( event ){
+$.mobile.changePage( "#pagina2", { transition: "slide", changeHash: false });
+  }
 });
 
 
+$(function(){
+  // Bind the swipeHandler callback function to the swipe event on div.box
+  $( "#pagina2" ).on( "swipeleft", swiperleftpagina2 );
+
+  // Callback function references the event target and adds the 'swipe' class to it
+  function swiperleftpagina2( event ){
+$.mobile.changePage( "#pizzas", { transition: "slide", changeHash: false });
+  }
+});
+
+$(function(){
+  // Bind the swipeHandler callback function to the swipe event on div.box
+  $( "#pagina2" ).on( "swiperight", swiperrightpagina2 );
+
+  // Callback function references the event target and adds the 'swipe' class to it
+  function swiperrightpagina2( event ){
+$.mobile.changePage( "#pagina3", { transition: "slide", changeHash: false });
+  }
+});
+
+
+$(function(){
+  // Bind the swipeHandler callback function to the swipe event on div.box
+  $( "#pagina3" ).on( "swipeleft", swiperlefpagina3 );
+
+  // Callback function references the event target and adds the 'swipe' class to it
+  function swiperlefpagina3( event ){
+$.mobile.changePage( "#pagina2", { transition: "slide", changeHash: false });
+  }
+});
+
+$(function(){
+  // Bind the swipeHandler callback function to the swipe event on div.box
+  $( "#pagina3" ).on( "swiperight", swiperrightpagina3 );
+
+  // Callback function references the event target and adds the 'swipe' class to it
+  function swiperrightpagina3( event ){
+$.mobile.changePage( "#pizzas", { transition: "slide", changeHash: false });
+  }
+});
 
 $("[data-role='collapsible']").collapsible({
 
